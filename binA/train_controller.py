01cd830a059c3d10b01d1e19292fd17e9a0afb0c
Deniz Germen <73364909+dGermen@users.noreply.github.com>
2023-06-18 22:29:56 +0300

    Made tests parametric

--- binA/train_controller.py
+++ binA/train_controller.py
@@ -18,7 +18,7 @@ class train_controller():
 
         if preferred_device == None:
             if torch.backends.mps.is_available():
-                preferred_device=       'mps'
+                preferred_device=       'cpu'
             elif torch.cuda.is_available():
                 preferred_device= 'cuda'
             else:
@@ -121,7 +121,7 @@ class train_controller():
             pos_logit = model.decode(z_train, pos_edge_index)
             neg_logit = model.decode(z_train, neg_edge_index)
 
-            loss = model.bpr_loss(pos_logit, neg_logit)
+            loss = models.bpr_loss(pos_logit, neg_logit)
             # append the loss to the train_losses
             train_losses.append(loss)
 
@@ -135,12 +135,12 @@ class train_controller():
             # Validation:
             if (epoch +1) % 5 == 0:
                 # validation function calls model.eval(), calculating both val loss & recall@50
-                val_loss = self.validation(model, z_train)
+                val_loss = self.validation(model = model, z = z_train)
                 # append the val loss to the val_losses
                 val_losses.append(val_loss)
                 print(f'Validation Loss: {val_loss}')
                 if test_active:
-                    res = self.test_transductive(model, z_train)
+                    res = self.test_transductive( z = z_train, model = model)
                     print("recall@50: ", res)
 
                 """
@@ -162,6 +162,104 @@ class train_controller():
 
         # save @ exit
         save_all(model, train_losses, val_losses, epochs, save_path)
+
+    def test_inductive(self,model = None, nodes = None, val_edges = None, k=50, sample_ratio=0.5, neg_coef=1600):
+        model.eval()  # Set the model to evaluation mode
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # add this line to set the device
+
+        # Take 5 samples from val_edges as positive examples
+        if model == None:
+            model = self.model
+
+        if nodes == None:
+            nodes = self.V_new
+
+        if val_edges == None:
+            val_edges = self.data_processor.E_val
+
+
+        with torch.no_grad():
+
+            # Embedding new nodes ------- DIFFERENT PART
+            z = model(z, self.data_processor.E_val)
+
+            # Convert V to a boolean tensor for faster lookup.
+            v_mask = torch.zeros(self.num_nodes, dtype=torch.bool)
+            v_mask[nodes] = True
+            v_mask = v_mask.to(device)
+
+            # Assume val_edges contains the validation edges (it should be a 2 x num_val_edges tensor)
+            # val_edges = ...
+
+            # Check if both nodes of each edge in val_edges are in V
+            source_nodes = val_edges[0, :]
+            target_nodes = val_edges[1, :]
+            can_exist_in_V = v_mask[source_nodes] & v_mask[target_nodes]
+
+            can_exist_in_V.to(device)
+
+            # Filter the edges that can exist in V
+            valid_edges_in_V = val_edges[:, can_exist_in_V].to(device)
+            positive_pairs = valid_edges_in_V
+
+
+            # FOR MEMORY
+            selected_pairs = positive_pairs[:, torch.randint(valid_edges_in_V.size(1), (int(sample_ratio*valid_edges_in_V.size(1)),))]
+
+
+            # --- Generating negative pairs ---
+
+            # Find the unique starting nodes in val_edges
+            start_nodes = torch.unique(selected_pairs[0, :]).to(device)
+
+            # Initialize a numpy array to keep node id and recall@50 for each node
+            recall_at_k = np.zeros((len(start_nodes), 2))
+
+            # Tour over start nodes
+            a = time.time()
+            for start_node in start_nodes:
+                timezzz = time.time()
+
+
+                all_possible_pairs = torch.stack(torch.meshgrid(start_node, self.V), dim=-1).reshape(-1, 2).t().to(device)
+                all_possible_pairs = all_possible_pairs[:, torch.randint(all_possible_pairs.size(1), (neg_coef*valid_edges_in_V.size(1),))]
+
+                # Clock time for look up and print it
+                start_node = int(start_node)
+                positive_pairs = self.start_node_dict_val[start_node] # THIS IS HARDCODED GLOBAL VARIABLE DO NOT COPY PASTE
+
+
+                # Remove the existing edges in val_edges from all_possible_pairs to create the negative pairs
+                existing_pairs = positive_pairs.t()
+                existing_pairs = existing_pairs.to(device)
+
+                # Removing positive pairs that are generated accidentaly
+                negative_pairs = remove_common_edges(E_all=positive_pairs,B=all_possible_pairs) # B - (A INTERSECTION B)
+
+                # Negative examples for validation
+                timezzzz = time.time()
+                positive_scores = model.decode(z, positive_pairs)
+                negative_scores = model.decode(z, negative_pairs)
+
+
+                # Combine positive edges and negative scores
+                all_edges = torch.cat([positive_pairs, negative_pairs], dim=1)
+                all_scores = torch.cat([positive_scores, negative_scores])
+                # Indicate which edges are positive (1 for positive, 0 for negative)
+                positive_edge_indicator = torch.tensor([1]*positive_pairs.size(1) + [0]*negative_pairs.size(1)).to(device)
+
+
+                recall= calculate_recall_per_node(all_edges, all_scores, positive_edge_indicator, k,start_node)
+                # Report passed time
+
+                recall_at_k[start_node, 0] = start_node
+                recall_at_k[start_node, 1] = recall
+
+                del all_possible_pairs
+                del all_scores
+                del all_edges
+
+            return recall, recall_at_k
 
 
     def validation(self,z,model =None, nodes =None, val_edges= None):
@@ -187,11 +285,11 @@ class train_controller():
             pos_logit = model.decode(z, pos_edge_index)
             neg_logit = model.decode(z, neg_edge_index)
 
-            val_loss = model.bpr_loss(pos_logit, neg_logit)
+            val_loss = models.bpr_loss(pos_logit, neg_logit)
 
         return val_loss.item()
     
-    def test_transductive(self,z,model = None, nodes = None, val_edges = None, k=50):
+    def test_transductive(self,z,model = None, nodes = None, val_edges = None, k=50, sample_ratio=0.5, neg_coef=1600):
         
         
 
@@ -230,7 +328,7 @@ class train_controller():
 
 
             # FOR MEMORY
-            selected_pairs = positive_pairs[:, torch.randint(valid_edges_in_V.size(1), (500,))]
+            selected_pairs = positive_pairs[:, torch.randint(valid_edges_in_V.size(1), (int(sample_ratio*valid_edges_in_V.size(1)),))]
 
 
             # --- Generating negative pairs ---
@@ -248,6 +346,7 @@ class train_controller():
 
 
                 all_possible_pairs = torch.stack(torch.meshgrid(start_node, self.V), dim=-1).reshape(-1, 2).t().to(device)
+                all_possible_pairs = all_possible_pairs[:, torch.randint(all_possible_pairs.size(1), (neg_coef*valid_edges_in_V.size(1),))]
 
                 # Clock time for look up and print it
                 start_node = int(start_node)
